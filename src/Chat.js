a193e530319e86149e08200cce037c90b817291f
Keertana Kota <keerko2218@gmail.com>
2018-07-03 14:08:00 -0400

    Added side bar and chat components

---
+++ src/Chat.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+class Chat extends Component{
+    render(){
+        return(
+            <div className = "Chat">
+            Chat
+            </div>
+        )
+    }
+}
+export default Chat
