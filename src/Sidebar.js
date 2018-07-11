d092fccad4014bd6e9f8496a1af875e241520946
Keertana Kota <keerko2218@gmail.com>
2018-07-11 13:55:56 -0400

    new room buttton will show up on sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,7 +3,7 @@ import React from 'react'
 import UserInfo from './UserInfo'
 import RoomList from './RoomList'
 
-const Sidebar = ({ user, signOut, rooms }) => {
+const Sidebar = ({ user, signOut, rooms, setCurrentRoom, showRoomForm }) => {
   return (
     <aside
       className="Sidebar"
@@ -14,9 +14,11 @@ const Sidebar = ({ user, signOut, rooms }) => {
         signOut={signOut}
       />
       <h1 style={styles.h1}>XTBC 18</h1>
-      <RoomList 
-      rooms={rooms}
-       />
+      <RoomList
+        rooms={rooms}
+        setCurrentRoom={setCurrentRoom}
+      showRoomForm={showRoomForm}
+      />
     </aside>
   )
 }
